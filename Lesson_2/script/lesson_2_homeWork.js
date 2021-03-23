9a1be7583e9fddea2e188174e04062849610c697
Peacefull-Bro <konevtsov.d@mail.ru>
2021-03-23 23:38:07 +0300

    Переписал функции в задании 4

--- Lesson_2/script/lesson_2_homeWork.js
+++ Lesson_2/script/lesson_2_homeWork.js
@@ -1,4 +1,3 @@
-
 /*Дополнительные видео перед выполнением домашнего задания посмотреть не успел, поэтому, возможно код написан не совсем эстетично.
 к следующему дз исправлюсь */
 
@@ -7,94 +6,84 @@
 
 
 // ПРИМЕР №1
-    let a = 1, b = 1, c, d;
-    c = ++a;
-    alert(c);
+let a = 1,
+    b = 1,
+    c, d;
+c = ++a;
+// alert(c);
 
 /*  ++ Это икремент он увеличивает значение переменной на 1,
     поэтому значение переменной с будет 2 */
 
 //ПРИМЕР №2
-    d = b++;
-    alert(d); 
+d = b++;
+// alert(d);
 
 /*  В случае применения оператора ++ качестве окончания он сначала возвратит значение переменной b,
     а потом добавит к ней единицу.*/
 
-    //ПРИМЕР №3
-    c = 2 + ++a;
-    alert(c); 
+//ПРИМЕР №3
+c = 2 + ++a;
+// alert(c);
 
-    /* Тут все легко, в переменной а было значение 2 с помощью икремента прибавилась еденица, получилось значение 3,
-    которое прибавилось к числу 2, на выходе получили 5*/
+/* Тут все легко, в переменной а было значение 2 с помощью икремента прибавилась еденица, получилось значение 3,
+которое прибавилось к числу 2, на выходе получили 5*/
 
-    //ПРИМЕР №4
-    d = 2 + b++;
-    alert(d); 
-    alert(a); 
-    alert(b); 
+//ПРИМЕР №4
+d = 2 + b++;
+// alert(d);
+// alert(a);
+// alert(b);
 
-    /* В этом примере инкремент стоит после переменной, как была сказано выше,
-    в этом случае значение переменной сначала вернется, и только потом прибавится,
-    поэтому получаем значение переменной d = 4, переменная а остается со значением 3, а переменная b = 3*/
+/* В этом примере инкремент стоит после переменной, как была сказано выше,
+в этом случае значение переменной сначала вернется, и только потом прибавится,
+поэтому получаем значение переменной d = 4, переменная а остается со значением 3, а переменная b = 3*/
 
 // ЗАДАНИЕ №2
-    // Изменил букву переменной а на z т.к. хром ругался на то что такая переменная уже существует
-    let z = 2;
-    let x = 1 + (z *= 2);
-    console.log(x);
+// Изменил букву переменной а на z т.к. хром ругался на то что такая переменная уже существует
+let z = 2;
+let x = 1 + (z *= 2);
+console.log(x);
 
-    /*так как значения в скобках имеют больший приоритет, поэтому сначала выполнилась операция умножения,
-    потом к результату прибавилась еденица, получилось 5*/
+/*так как значения в скобках имеют больший приоритет, поэтому сначала выполнилась операция умножения,
+потом к результату прибавилась еденица, получилось 5*/
 
 // ЗАДАНИЕ №3
-    let numberOne = 7;
-    let numberTwo = 2;
-    
-    if (numberOne >= 0, numberTwo >= 0) {
-        console.log(numberOne - numberTwo);
-    } else if (numberOne < 0, numberTwo < 0) {
-        console.log(numberOne * numberTwo);
-    } else if (numberOne < 0, numberTwo >= 0) {
-        console.log+(numberOne + numberTwo);
-    } 
+let numberOne = 7;
+let numberTwo = 2;
 
-    // ЗАДАНИЕ №4
+if (numberOne >= 0, numberTwo >= 0) {
+    console.log(numberOne - numberTwo);
+} else if (numberOne < 0, numberTwo < 0) {
+    console.log(numberOne * numberTwo);
+} else if (numberOne < 0, numberTwo >= 0) {
+    console.log + (numberOne + numberTwo);
+}
 
-    // Функция сложения
-    function numPlus() {
-        let numA = 15;
-        let numB = 5;
-        let result = numA + numB;
-        return result;
-    }
-    console.log(numPlus());
+// ЗАДАНИЕ №4
 
-    // Функция вычитания
-    function numMinus() {
-        let numA = 15;
-        let numB = 5;
-        let result = numA - numB;
-        return result;
-    }
-    console.log(numMinus());
+// Функция сложения
+function numPlus(numA, numB) {
+    return numA + numB;
+}
+console.log(numPlus(15, 5));
 
-    // Функция деления
-    function numDivision() {
-        let numA = 15;
-        let numB = 5;
-        let result = numA / numB;
-        return result;
-    }
-    console.log(numDivision());
+// Функция вычитания
+function numMinus(numA, numB) {
+    return numA - numB;
+}
+console.log(numMinus(15, 5));
 
-    // Функция умножения
-    function numMultiplying() {
-        let numA = 15;
-        let numB = 5;
-        let result = numA * numB;
-        return result;
-    }
-    console.log(numMultiplying());
+// Функция деления
+function numDivision(numA, numB) {
+    return numA / numB;
+}
+console.log(numDivision(15, 5));
 
-    ЗАДАНИЕ №5
+// Функция умножения
+function numMultiplying(numA, numB) {
+    return numA * numB;
+}
+console.log(numMultiplying(15, 5));
+
+//ЗАДАНИЕ №5
